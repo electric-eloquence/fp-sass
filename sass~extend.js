7db190d0d5138e3bff6f8e1e91489aedd7537837
e2tha-e <ee@electric-eloquence.io>
2016-12-15 10:00:50 -0500

    more corrections

--- sass~extend.js
+++ sass~extend.js
@@ -6,11 +6,10 @@ const plugins = require('gulp-load-plugins')();
 const runSequence = require('run-sequence');
 
 const appDir = global.appDir;
-const utilsCore = require(`${appDir}/core/lib/utils`);
-const utilsTask = require(`${appDir}/tasker/utils`);
+const utils = require(`${appDir}/core/lib/utils`);
 
-const cssBldDir = utilsCore.pathResolve(conf.ui.paths.source.cssBld);
-const cssSrcDir = utilsCore.pathResolve(conf.ui.paths.source.cssSrc);
+const cssBldDir = utils.pathResolve(conf.ui.paths.source.cssBld);
+const cssSrcDir = utils.pathResolve(conf.ui.paths.source.cssSrc);
 
 gulp.task('sass', function () {
   return gulp.src(cssSrcDir + '/sass/*.s(a|c)ss')
